9daaa9e980c7c4e56938edba015bca07242f8e61
Gerardo Perez Rocha <gerardoperezrocha@gmail.com>
2022-12-14 22:04:00 -0600

    tests passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,18 @@
-const repeatString = function() {
+
+const repeatString = function(string, num) {
+let newString = '';
+let i = 0;
+if (num < 0 ) {return 'ERROR'} 
+else
+   
+    for (i = 0; i < num  ; i++)   {
+
+       newString = newString + string;
+      
+}
+ 
+ return newString;
+
 
 };
 
