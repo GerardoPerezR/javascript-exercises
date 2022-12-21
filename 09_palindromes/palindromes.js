be37f7eede0d64691359d3af23971c4ade419d41
Gerardo Perez Rocha <gerardoperezrocha@gmail.com>
2022-12-21 15:51:49 -0600

    TESTS PASSED trickier than expected...had to go into RegEx to solve this. Is there any other way?

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
+const palindromes = function (myString) {
 
+    myString = myString.replace(/[.,\/#!$%\^&\*;:{}=' '\-_`~()]/g,"").toLowerCase();
+
+    const myArray = Array.from(myString);
+
+    console.log(myArray);
+
+const reversedArray = myArray.reverse();
+
+console.log(reversedArray);
+let myReverseString = reversedArray.join('');
+
+if (myReverseString === myString)   {
+
+    result = true;
+}
+ else   {
+    result = false;
+ }
+return result;
 };
 
 // Do not edit below this line
