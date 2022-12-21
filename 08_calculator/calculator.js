f17fd350ca53e6e77d3d84b10a3ec06b6d751d6e
Gerardo Perez Rocha <gerardoperezrocha@gmail.com>
2022-12-21 15:13:15 -0600

    TESTS PASSED well, this was a bit trickier than expected. Learned that NPM is not ES6 compatible

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,67 @@
-const add = function() {
+const add = function(x, y) {
+
+      console.log(x, y);
+      result = x + y;
+      return result;
+
+
+
 	
 };
 
-const subtract = function() {
+const subtract = function(x, y) {
+
+  console.log(x, y);
+      result = x - y;
+      return result;
+
 	
 };
 
-const sum = function() {
+const sum = function(myArgs) {
+
+  console.log(myArgs);
+  var result = 0;
+  for (var i = 0; i < myArgs.length; i++)  {
+    result += myArgs[i];
+  }
+  console.log(result);
+  return result;
 	
 };
 
-const multiply = function() {
+const multiply = function(myArgs) {
+  console.log(myArgs);
+  var result = 1;
+  for (var i = 0; i < myArgs.length; i++)  {
+    result *= myArgs[i];
+  }
+  console.log(result);
+  return result;
 
 };
 
-const power = function() {
+const power = function(x, y) {
+      result = 1;
+  for (i=0; i<y; i++) {
+     
+      result = result * x;
+  
+
+  }
+	    return result;
+};
+
+const factorial = function(x) {
+
+  console.log(x);
+  var result = 1;
+  for (var i = 1; i <= x; i++)  {
+    result *= i;
+  }
+  console.log(result);
+  return result;
 	
-};
-
-const factorial = function() {
 	
 };
 
