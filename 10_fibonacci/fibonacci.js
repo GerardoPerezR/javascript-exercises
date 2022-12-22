9b770fb2458d8c154f92dc3ed80c98dd8357bf38
Gerardo Perez Rocha <gerardoperezrocha@gmail.com>
2022-12-22 12:15:25 -0600

    TESTS PASSED This was tricky

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
+    var a = 1;
+    var b = 1;
+
+    if (x<0)    {return "OOPS"};
+  
+
+    var result = 1;
+    for (i = 2; i < x; i++) {
+
+      result = a + b;
+      a = b;
+      b = result;
+
+    }
+
+
+  console.log(result);
+return result;
+
 
 };
 
