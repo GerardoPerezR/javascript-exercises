10ab628f138c63b1fa913d54138a061736a39f36
Gerardo Perez Rocha <gerardoperezrocha@gmail.com>
2023-01-11 16:20:54 -0600

    DONE

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,45 @@
-const findTheOldest = function() {
+const findTheOldest = (myArray)=> {return myArray.reduce(  (a,b) =>  {
 
-};
+    
+
+    const aAge = getAge(a.yearOfBirth, a.yearOfDeath); 
+    const bAge = getAge(b.yearOfBirth, b.yearOfDeath);
+    
+
+    if (aAge < bAge )   {
+        return b
+    }
+    else{return a}
+
+
+console.log(getAge());
+
+
+
+
+
+
+} 
+
+
+    //compare ages (sort)
+
+    
+
+    //return oldes
+
+
+
+
+
+)}
+;const getAge = ((birth,finalDate) => { 
+    
+    if (!finalDate) {
+        finalDate = (new Date()).getFullYear();
+    }
+  
+    return finalDate - birth })
 
 // Do not edit below this line
 module.exports = findTheOldest;
